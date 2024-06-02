ff246ff3d3d0973ec5a7e2399df3ea002fde0712
Lukas Calmbach <lcalmbach@gmail.com>
2024-06-02 10:27:54 +0200

    remote secret key

--- src/teach_match/settings.py
+++ src/teach_match/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(mm=-tr1g8w2x&+ch5%ykfpgo7mvti#4fcfy6t!%v8$7f7r4jo"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'lthz&jt*rosx3+04vb$&+gx@cl_o494e_*f#%3u_@ha34i3y^&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
